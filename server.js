2b2a5898d2f3628125dee75da0e5a6eb2758e8bb
yuval.shlomo <yuval8992@gmail.com>
2021-06-02 19:28:16 +0300

    add server

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require("express");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/weather-app"));
+app.use(express.static("./dist/trivia-game"));
 
 app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/weather-app/" });
+  res.sendFile("index.html", { root: "dist/trivia-game/" });
 });
 
 // Start the app by listening on the default Heroku port
